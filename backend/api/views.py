b92c288875584d2664a7e02967d5d7af389c8fcb
h-hamakawa <fko2447005@stu.o-hara.ac.jp>
2025-10-10 14:16:12 +0900

    じゅｔｙれいうｔｙふいｊｇｈｃｒといｊふゆふいｇｆｔｒｇひｌ

---
+++ backend/api/views.py
@@ -0,0 +1,7 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(["GET"])
+def hello(request):
+    data = {"message": "川久保4ね"}
+    return Response(data)
