0488d1bae83b0d2215b0be740f029937a3e1c52e
h-hamakawa <fko2447005@stu.o-hara.ac.jp>
2025-10-20 11:55:59 +0900

    k

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -19,6 +19,7 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
+
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=50, blank=True)
