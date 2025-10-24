69b2e173ef26bd99fefcbbe624fddcd59a80c0cd
k-kawakubo0924 <fko2447056@stu.o-hara.ac.jp>
2025-10-24 10:53:56 +0900

    肌質ごとの成分まとめ

--- backend/recommend/models.py
+++ backend/recommend/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 from Skin.models import Skin_type
+
 # Create your models here.
 
+
 class Ingredients(models.Model):
-    INGREDIENTS_NAME = models.CharField(max_length=40,primary_key=True)
+    INGREDIENTS_NAME = models.CharField(max_length=40, primary_key=True)
     COMMENT = models.TextField()
+
     def __str__(self):
         return self.INGREDIENTS_NAME
-    
+
 
 class appropriate(models.Model):
     appropriate = models.BooleanField(null=True)
-    INGREDIENTS_NAME = models.ForeignKey(Ingredients, on_delete=models.CASCADE)  
-    type_name = models.ForeignKey(Skin_type,on_delete=models.CASCADE)
-    def __str__(self):
-        return self.appropriate
+    INGREDIENTS_NAME = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+    type_name = models.ForeignKey(Skin_type, on_delete=models.CASCADE)
