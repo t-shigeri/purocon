be547c66c6e9c57f7ba4450ae4f2ccc736353123
k-kawakubo0924 <fko2447056@stu.o-hara.ac.jp>
2025-11-07 14:17:51 +0900

    logout機能実装

--- team-app/src/admin/Ingredient/Ingredient.jsx
+++ team-app/src/admin/Ingredient/Ingredient.jsx
@@ -1,3 +1,13 @@
+import Logout from "../../components/logout";
+import "../AdminPage.css";
+
 export default function Ingredient() {
-  return <div>Ingredient</div>;
+  return (
+    <>
+      <div>Ingredient</div>
+      <div className="logout-button-container">
+        <Logout />
+      </div>
+    </>
+  );
 }
