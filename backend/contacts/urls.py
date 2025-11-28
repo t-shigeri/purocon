437b00004077e3a4515bd44206275898cf31bd6d
h-hamakawa <fko2447005@stu.o-hara.ac.jp>
2025-11-28 10:46:20 +0900

    レコメンド

--- backend/contacts/urls.py
+++ backend/contacts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ContactsInputView
  
 urlpatterns = [
-    path("text/", ContactsInputView.as_view(), name="Contacts"),
+    path("return/", ContactsInputView.as_view(), name="Contacts"),
 ]
